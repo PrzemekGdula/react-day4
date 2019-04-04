30851d928a94a4949b547d7cf2c78c064732399c
PrzemekGdula <przemo.g12@wp.pl>
2019-04-04 12:38:29 +0200

    user with avatar and email

--- src/List/Results/ResultsItem.js
+++ src/List/Results/ResultsItem.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
 
-const ResultsItem = (props) => (
-    <div>
-        {props.user.name.first} {props.user.name.last}
-    </div>
-)
+
+const ResultsItem = ({ user }) => {
+    const name = user.name.first + ' ' + user.name.last
+
+    return (
+        <ListItem alignItems="flex-start"
+        >
+            <ListItemAvatar>
+                <Avatar
+                    alt={name}
+                    src={user.picture.medium}
+                />
+            </ListItemAvatar>
+            <ListItemText
+                primary={name}
+                secondary={user.email}
+            />
+        </ListItem>
+    )
+}
 
 export default ResultsItem
